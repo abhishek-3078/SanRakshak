78306ade2fc31c9449e75e1aa14b2fe82dfb7a98
aman <12213069@nitkkr.ac.in>
2023-09-20 00:59:49 +0530

    one page part

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,20 +4,21 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import NavSide from './components/NavSide';
 import Navbar from './components/Navbar';
+import ImportantInfoBox from './components/ImportantInfoBox';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='bg-white text-blue-400 '>
+      <div className=''>
           <section>
               <Navbar></Navbar>
           </section>
           <section className='flex'>
               <NavSide></NavSide>
-              <div>
-                  
+              <div className='flex justify-center w-full mt-3'>
+                  <ImportantInfoBox></ImportantInfoBox>
               </div>
           </section>
       </div>
---
+++ frontend/src/components/ImportantMsg.jsx
@@ -0,0 +1,8 @@
+function ImportantMsg({msg}){
+    return(
+        <div className="flex justify-center items-center w-full text-white border-2 border-solid">
+            {msg}
+        </div>
+    )
+}
+export default ImportantMsg;

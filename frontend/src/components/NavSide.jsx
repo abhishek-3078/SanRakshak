214f95e80e94b766f2523e223bd39f1e82f260ac
aman <12213069@nitkkr.ac.in>
2023-09-19 23:13:00 +0530

    start work

---
+++ frontend/src/components/NavSide.jsx
@@ -0,0 +1,34 @@
+import ButtonTransparent from "./ButtonTransparent";
+function NavSide() {
+    return (
+        <div className="bg-[#F56868] w-[15vw] h-[90.5vh]">
+            <ul className="flex flex-col w-full">
+                <li className="flex ml-2 space-x-5 my-5">
+                    <img className="w-6 h-6 invert-[1]" src="https://cdn-icons-png.flaticon.com/128/3524/3524878.png" alt="" />
+                    <ButtonTransparent>Dashboard</ButtonTransparent>
+                </li>
+                <li className="flex ml-2 space-x-5 my-5">
+                    <img className="w-6 h-6 invert-[1]" src="https://cdn-icons-png.flaticon.com/128/1247/1247944.png" alt="" />
+                    <ButtonTransparent>Report /<br/>Complaint</ButtonTransparent>
+                </li>
+                <li className="flex ml-2 space-x-5 my-5">
+                    <img className="w-6 h-6 invert-[1]" src="https://cdn-icons-png.flaticon.com/128/497/497789.png" alt="" />
+                    <ButtonTransparent>Alerts</ButtonTransparent>
+                </li>
+                <li className="flex ml-2 space-x-5 my-5">
+                    <img className="w-6 h-6 invert-[1]" src="https://cdn-icons-png.flaticon.com/128/1542/1542858.png" alt="" />
+                    <ButtonTransparent>Public Forum</ButtonTransparent>
+                </li>
+                <li className="flex ml-2 space-x-5 my-5">
+                    <img className="w-6 h-6 invert-[1]" src="https://cdn-icons-png.flaticon.com/128/9975/9975755.png" alt="" />
+                    <ButtonTransparent>What to do ?</ButtonTransparent>
+                </li>
+                <li className="flex ml-2 space-x-5 my-5">
+                    <img className="w-6 h-6 invert-[1]" src="https://cdn-icons-png.flaticon.com/128/1828/1828479.png" alt="" />
+                    <ButtonTransparent>Log Out</ButtonTransparent>
+                </li>
+            </ul>
+        </div>
+    )
+}
+export default NavSide;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+function Navbar(){
+    return(
+        <div className="bg-[#F56868] h-14 w-screen flex items-center justify-end">
+            <img className="w-10 h-10 mr-10" src="https://cdn-icons-png.flaticon.com/128/64/64572.png" alt="userimg" />
+        </div>
+    )
+}
+export default Navbar;

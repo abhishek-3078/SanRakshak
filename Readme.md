b642d81686de409a23e4cdc5e5584f5b25e1f7a1
Abhishek <12213078@nitkkr.ac.in>
2023-09-19 21:16:38 +0530

    init commit

---
+++ Readme.md
@@ -0,0 +1,1 @@
+## SanRakshap App
---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='bg-white text-blue-400 w-[400px] h-[400px]'>
+j
+      </div>
+    </>
+  )
+}
+
+export default App

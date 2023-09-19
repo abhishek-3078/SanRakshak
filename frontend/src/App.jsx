6539559521f5b9792d74ac6e15c7fd1961759802
archit <architchhajed6@gmail.com>
2023-09-20 01:07:27 +0530

    carefull while merging file names altered

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,20 +4,26 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import NavSide from './components/NavSide';
 import Navbar from './components/Navbar';
+import MissingPeopleDash from './components/MissingPeopleDash';
+import UserMap from './components/UserMap';
+import ImportantInfo from './components/ImportantInfo';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='bg-white text-blue-400 '>
+      <div className=''>
           <section>
               <Navbar></Navbar>
           </section>
           <section className='flex'>
               <NavSide></NavSide>
-              <div>
-                  
+              <div className='flex flex-col'>
+                <ImportantInfo></ImportantInfo>
+                  {/* <MissingPeopleDash /> */}
+                  {/* <UserMap /> */}
               </div>
           </section>
       </div>

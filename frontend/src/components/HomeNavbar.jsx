ef699a19e45e4a21e2c067280823da2b6e605ceb
aman <12213069@nitkkr.ac.in>
2023-09-20 12:01:22 +0530

    no change

--- frontend/src/components/HomeNavbar.jsx
+++ frontend/src/components/HomeNavbar.jsx
@@ -3,9 +3,11 @@ import { Link } from "react-router-dom";
 function HomeNavbar({color}){
     return(
         <div className={`bg-[${color}] h-14 w-screen flex items-center justify-between `}>
-            <h1 className="font-bold text-white text-[30px] mx-10 font-serif">
+            <Link to={'/'}>
+                <h1 className="font-bold text-white text-[30px] mx-10 font-serif">
                 Sanrakshak
-            </h1>
+                </h1>
+            </Link> 
             <div className="flex  items-center">
                 <button className="mx-10 text-white bg-[#F56868] p-3 rounded-lg hover:bg-green-400">LogIn</button>
                 <img className="w-10 h-10 mr-10 " src="https://cdn-icons-png.flaticon.com/128/64/64572.png" alt="userimg" />

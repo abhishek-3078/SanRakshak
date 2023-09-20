e83b53112badd53bda24f84b1d556422666807ae
Yash3115 <135955323+Yash3115@users.noreply.github.com>
2023-09-20 12:03:02 +0530

    sign up page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,23 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import NavSide from './components/NavSide';
 import Navbar from './components/Navbar';
+import SignUp from './components/SignUp';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='bg-white text-blue-400 '>
-          <section>
-              <Navbar></Navbar>
-          </section>
-          <section className='flex'>
-              <NavSide></NavSide>
-              <div>
-                  
-              </div>
-          </section>
-      </div>
+    
     </>
   )
 }

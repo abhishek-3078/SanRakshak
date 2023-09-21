dc76b4440d1d260b2f393f3715c672720fc480ae
Garvit <garvitg0108@gmail.com>
2023-09-21 10:27:10 +0530

    minor changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,13 +2,12 @@ const express=require('express')
 const jwt=require('jsonwebtoken')
 require('dotenv').config()
 require('./db/mongoose')
-const authRoute = require("./routers/auth");
-const passport = require("passport");
-const passportStrategy = require("./passport");
+// const authRoute = require("./routers/auth");
+// const passport = require("passport");
 const bodyParser = require('body-parser');
 
 
-const cookieSession = require("cookie-session");
+// const cookieSession = require("cookie-session");
 const userRouter=require('./routers/user')
 const cors=require('cors')
 // const taskRouter=require('./routers/task')
@@ -20,18 +19,18 @@ app.use(cors({
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
 }))
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(
-	cookieSession({
-		name: "session",
-		keys: ["cyberwolve"],
-		maxAge: 24 * 60 * 60 * 100,
-	})
-);
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(
+// 	cookieSession({
+// 		name: "session",
+// 		keys: ["cyberwolve"],
+// 		maxAge: 24 * 60 * 60 * 100,
+// 	})
+// );
 
 
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 app.use(express.json()) 
 app.use(express.urlencoded({ extended: true }))
@@ -42,7 +41,7 @@ app.get("/",(req,res)=>{
 })
 
 app.use(userRouter)
-app.use("/auth", authRoute)
+// app.use("/auth", authRoute)
 
 
   
--- backend/src/routers/user.js
+++ backend/src/routers/user.js
@@ -31,37 +31,38 @@ router.post('/user/signup',async(req,res)=>{
     }
     
 })
-router.post('/user/login',passport.authenticate('local',{failureRedirect:"/auth/login/failed",successRedirect:process.env.CLIENT_URL}));
-// router.post('/user/login',async(req,res)=>{
-//     let userData=req.body
-//     console.log(userData)
-//     try{
-//         try{
+// router.post('/user/login',passport.authenticate('local',{failureRedirect:"/auth/login/failed",successRedirect:process.env.CLIENT_URL}));
+router.post('/user/login',async(req,res)=>{
+    let userData=req.body
+    console.log(userData)
+    try{
+        try{
 
-//             const user=await User.findByCredentials(req.body.email,req.body.password)
-//             const token=await user.generateAuthToken()
-//             res.send({user,token})
-//         }catch(e){
-//             res.status(400).send("error"+ e)
-//         }
-//     }catch(e){
-//         res.status(403).send({success:false,message:e.message})
-//     }
-// })
-router.post('/user/logout',(req,res)=>{
-    console.log("ask for logout",req.session)
-    req.logout()
-    res.redirect(process.env.CLIENT_URL)
-    
+            const user=await User.findByCredentials(req.body.email,req.body.password)
+            const token=await user.generateAuthToken()
+            res.send({user,token})
+        }catch(e){
+            res.status(400).send("error"+ e)
+        }
+    }catch(e){
+        res.status(403).send({success:false,message:e.message})
+    }
 })
-// router.get('/user/logout',(req,res)=>{
-//     try{
-//         console.log("hello");
-//         res.send({success:true,message:"successfully logout"})
-//     }catch(e){
-//         console.log(e)
-//     }
-   
+
+// router.post('/user/logout',(req,res)=>{
+//     console.log("ask for logout",req.session)
+//     req.logout()
+//     res.redirect(process.env.CLIENT_URL)
+    
 // })
+
+router.get('/user/logout',(req,res)=>{
+    try{
+        console.log("hello");
+        res.send({success:true,message:"successfully logout"})
+    }catch(e){
+        console.log(e)
+    }
+})
 
 module.exports=router

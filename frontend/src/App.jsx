1464ac92e5616c03949d469fd7b27fb65485afae
aman <12213069@nitkkr.ac.in>
2023-09-22 19:22:24 +0530

    AdminAddress component with map location done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import NavSide from './components/NavSide';
-import Navbar from './components/Navbar';
-import ImportantInfoBox from './components/ImportantInfoBox';
 import RescueServicesPage from './pages/RescueServicesPage';
 import Home from './pages/Home';
-import {Route,BrowserRouter as Router,Routes} from "react-router-dom";
-import ShelterForm from './components/ShelterForm'
-import AddLocationOnMap from './components/AddLocationOnMap';
 import AddShelterPage from './pages/AddShelterPage';
-import BurgerContext from './contexts/BurgerContext';
 import Login from './Login'
 import SignUp from './components/SignUp'
 import UserAlert from './pages/UserAlert'
-
-import UserMap from './components/UserMap'
-import MissingPeopleDash from './components/MissingPeopleDash';
-import SignUp from './components/SignUp'
-import Home from './pages/Home'
 import {BrowserRouter as Router , Routes,Route} from 'react-router-dom';
+import AdminAddress from './components/AdminAddress';
 function App() {
   const [count, setCount] = useState(0)
   return (
-
     <div>
-    <Router>
-    <Routes>
-      
+    {/* <Router>
+    <Routes> 
       <Route path="/login" element={<Login/>}></Route>
       <Route path="/signup" element={<SignUp/>}></Route>
       <Route path="/alerts" element={<UserAlert/>}></Route>
       <Route path="/" element={<Home/>}/>
       <Route path="/dashboard" element={<RescueServicesPage/>}/>
       <Route path="/addShelter" element={<AddShelterPage/>}/>
-      
+
     </Routes>
-    </Router>
+    </Router> */}
+    <AdminAddress/>
     </div>
     // <SignUp/>
   )
---
+++ frontend/src/components/AdminAddress.jsx
@@ -0,0 +1,139 @@
+import React, { useState } from "react";
+import { Loader } from "@googlemaps/js-api-loader";
+
+function AdminAddress() {
+
+    const [formMode, setFormMode] = useState(true);
+
+    function handleMarkLocation(e) {
+
+        setFormMode(false);
+        e.preventDefault();
+        const formData = Array.from(document.getElementsByClassName("data"));
+
+        const streetname = formData[0].value;
+        const streetnumber = formData[1].value;
+        const pincode = formData[2].value;
+        const city = formData[3].value;
+        const distict = formData[4].value;
+        const state = formData[5].value;
+        const country = formData[6].value;
+
+        console.log("streetname: " + streetname);
+        const completeAddress = streetname + ',' + streetnumber + ',' + pincode + ',' + city + ',' + distict + ',' + state + ',' + country;
+        console.log(completeAddress);
+
+        const mapLocationKey = `https://maps.googleapis.com/maps/api/geocode/json?address=${completeAddress}&key=AIzaSyDDAbFDKGmZpbRF_mBU0DjUG2kChD4ZGGw`
+
+        try {
+            fetch(mapLocationKey)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    return response.json(); // Assuming the response is in JSON format
+                })
+                .then(data => {
+                    // Work with the data here
+                    let mydata = data;
+                    console.log("data : ", data);
+                    const coordinates = mydata.results[0].geometry.location;
+                    console.log("my:", coordinates);
+
+                    const fun = async () => {
+                        const loader = new Loader({
+                            apiKey: "AIzaSyDDAbFDKGmZpbRF_mBU0DjUG2kChD4ZGGw",
+                            version: "weekly",
+                            libraries: ["places"],
+                        });
+
+                        // Request needed libraries.
+                        const { Map, InfoWindow } = await loader.importLibrary("maps");
+                        const { AdvancedMarkerElement } = await loader.importLibrary("marker");
+                        const map = new Map(document.getElementById("map"), {
+                            center: { lat: coordinates.lat, lng: coordinates.lng },
+                            zoom: 15,
+                            disableDoubleClickZoom: true,
+                            mapId: "4504f8b37365c3d0",
+                        });
+                        console.log("hello:",map)                     
+                        const infoWindow = new InfoWindow();
+                        const draggableMarker = new AdvancedMarkerElement({
+                            map,
+                            position: { lat: coordinates.lat, lng: coordinates.lng },
+                            gmpDraggable: true,
+                            title: "This marker is draggable.",
+                        });
+                        draggableMarker.addListener("dragend", (event) => {
+                            const position = draggableMarker.position;
+                            // alert("hello")
+                            console.log("hllo", position, position.lat)
+                            // infoWindow.close();
+                            infoWindow.setContent(
+                                `
+                        <div style="padding:10px">
+                        Pin dropped at: ${position.lat} , ${position.lng}
+                        </div>`
+                            );
+                            infoWindow.open(draggableMarker.map, draggableMarker);
+                        });
+
+       
+
+                    }
+
+                    fun();
+
+                })
+                .catch(error => {
+                    // Handle errors here
+                    console.error('Fetch error:', error);
+                });
+        } catch (error) {
+            // Handle exceptions that occur outside of the fetch block
+            console.error('An exception occurred outside of fetch:', error);
+        }
+
+    }
+    return (
+        <>
+            {formMode ? <section className="border-2 border-solid border-[#C5C5C5] w-screen rounded-xl h-screen px-10 py-10 overflow-auto">
+                <div>
+                    <h1 className="text-[25px] font-bold">
+                        Address Details
+                    </h1>
+                    <p>
+                        Add the address of your head office
+                    </p>
+                </div>
+                <form onSubmit={handleMarkLocation} className="my-5 myForm" action="">
+                    <div className="flex flex-col space-y-5 justify-center">
+                        <input required id="streetname" name="streetname" type="text" placeholder="Street name" className="border-2 data border-solid border-[#C5C5C5] px-3 rounded-lg" />
+                        <input required name="streetnumber" type="tel" placeholder="Street number" className="border-2 data border-solid border-[#C5C5C5] px-3 rounded-lg" />
+
+                        <input required name="pincode" type="tel" placeholder="pincode" className="border-2 data border-solid border-[#C5C5C5] px-3 rounded-lg" />
+                        <input required name="city" type="text" placeholder="city" className="border-2 data border-solid border-[#C5C5C5] px-3 rounded-lg" />
+                        <input required name="distict" type="text" placeholder="distict" className="border-2 data border-solid border-[#C5C5C5] px-3 rounded-lg" />
+
+                        <input required name="state" type="text" placeholder="state" className="border-2 data border-solid border-[#C5C5C5] px-3 rounded-lg" />
+                        <input required name="country" type="text" placeholder="country" className="border-2  data border-solid border-[#C5C5C5] px-3 rounded-lg" />
+                        <button className="border-2 border-solid border-[#C5C5C5] bg-[#6A8BFF] text-white py-1 rounded-md">Confirm Address</button>
+                    </div>
+
+                </form>
+            </section> :
+                <div className="flex flex-col items-center border-2 border-solid border-[#C5C5C5] w-screen rounded-xl h-screen px-10 py-10 overflow-auto">
+                    {/* hello */}
+                    <div className="font-bold text-[25px] my-5 ">
+                        Try to Mark the exact location , if correct leave as is it
+                    </div>
+                    <div id="map" className="w-full h-[80vh]"></div>
+                    <div className="w-full mx-auto my-5 flex justify-center space-x-[200px]">
+                        <button onClick={() => setFormMode(true)} className="border-2 border-solid border-[#C5C5C5] px-10 rounded-lg text-white bg-[#6c6cbc]">Edit</button>
+                        <button className="border-2 border-solid border-[#C5C5C5] px-10 rounded-lg text-white bg-[#ab4545]" >Confirm</button>
+                    </div>
+                </div>}
+        </>
+    )
+}
+export default AdminAddress;

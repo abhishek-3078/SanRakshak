a641e57b66195084b991afc4e6caaf1b62ebbe3b
Abhishek <12213078@nitkkr.ac.in>
2023-09-23 10:51:22 +0530

    Added community forum

--- frontend/src/components/HomeMidContent.jsx
+++ frontend/src/components/HomeMidContent.jsx
@@ -10,8 +10,8 @@ export default function HomeMidContent() {
         Here's how we can help
       </div>
       <div className='flex flex-row justify-evenly w-3/4 mx-auto'>
-        <div className='border-2 border-solid border-gray-400 rounded-md w-1/4 py-2 px-6 hover:scale-105 duration-200 hover:shadow-lg shadow-md shadow-gray-500'>
-            <div className='my-1'>
+        <div className='border-2 border-solid border-gray-400 rounded-md w-1/4 py-2 px-6 hover:scale-105 duration-200 hover:shadow-lg shadow-md shadow-gray-500' data-aos="zoom-in" data-aos-delay="100" >
+            <div className='my-1' >
                 <AiOutlineAlert size={40}></AiOutlineAlert>
                 <h1 className='text-lg mt-3 font-semibold'>Early Warning and Alerts</h1>                
             </div>
@@ -19,7 +19,7 @@ export default function HomeMidContent() {
                 <p className='text-gray-700'>The app can provide real-time alerts and early warnings about impending disasters. Users can receive notifications about weather conditions, geological activity, or other potential threats in their area, allowing them to take proactive measures.</p>
             </div>
         </div>
-        <d className='border-2 border-solid border-gray-400 rounded-md w-1/4 py-2 px-6 hover:scale-105 duration-200 hover:shadow-lg shadow-md shadow-gray-500'>
+        <d className='border-2 border-solid border-gray-400 rounded-md w-1/4 py-2 px-6 hover:scale-105 duration-200 hover:shadow-lg shadow-md shadow-gray-500' data-aos="zoom-in" data-aos-delay="330">
             <div className='my-1'>
                 <SiCrowdsource size={40}></SiCrowdsource>
                 <h1 className='text-lg mt-3 font-semibold'>Crowdsourced Data</h1>                
@@ -28,7 +28,7 @@ export default function HomeMidContent() {
                 <p className='text-gray-700'>The app can collect crowdsourced data from users on road conditions, flooding, and other critical information, which can be used to update maps and inform emergency response efforts.</p>
             </div>
         </d>
-        <d className='border-2 border-solid border-gray-400 rounded-md w-1/4 py-2 px-6 hover:scale-105 duration-200 hover:shadow-lg shadow-md shadow-gray-500'>
+        <d className='border-2 border-solid border-gray-400 rounded-md w-1/4 py-2 px-6 hover:scale-105 duration-200 hover:shadow-lg shadow-md shadow-gray-500' data-aos="zoom-in" data-aos-delay="560">
             <div className='my-1'>
                 <GrEmergency size={40}></GrEmergency>
                 <h1 className='text-lg mt-3 font-semibold'>Emergency Services Coordination</h1>                

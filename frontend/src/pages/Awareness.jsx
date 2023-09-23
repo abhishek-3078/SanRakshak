21897862a4e73a55f06fc03ddff1c59192195515
Yash3115 <135955323+Yash3115@users.noreply.github.com>
2023-09-23 13:47:05 +0530

    awareness pop up

--- frontend/src/pages/Awareness.jsx
+++ frontend/src/pages/Awareness.jsx
@@ -6,7 +6,7 @@ import Footer from '../components/Footer'
 export default function Awareness() {
   return (
     <div>
-      {/* <MainNav/> */}
+      <MainNav/>
       <AwarenessCards/>
       <Footer/>
     </div>
